e18e1dbe14f74730439ee5f757c2a7455c2eb488
j-adie456980 <justingoesforever@gmail.com>
2022-11-10 01:10:02 -0500

    minor tweaks to table display

--- displayData.py
+++ displayData.py
@@ -1,17 +1,17 @@
 import sqlite3
 
 def displayData():
-  print("\nDisplaying data . . . ")
+  print("\nDisplaying data . . . \n")
   conn = sqlite3.connect("example.db")
   cursor = conn.cursor()
   cursor.execute("SELECT * FROM chromosomes")
-  print(cursor.fetchall())
+  print("Chromosome Table:  ", cursor.fetchall())
   cursor.execute("SELECT * FROM interactions")
-  print(cursor.fetchall())
+  print("Interaction Table: ", cursor.fetchall())
   cursor.execute("SELECT * FROM loci")
-  print(cursor.fetchall())
+  print("Locus Table:       ", cursor.fetchall())
   cursor.execute("SELECT * FROM genes")
-  print(cursor.fetchall())
+  print("Gene Table:        ", cursor.fetchall())
   cursor.execute("SELECT * FROM motifs")
-  print(cursor.fetchall())
+  print("Motif Table:       ", cursor.fetchall())
   conn.close()
